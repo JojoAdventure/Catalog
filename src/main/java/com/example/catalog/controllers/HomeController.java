c9d9806811439936e65bdc182e1a8ca721d94327
Jojo <joe.regan285@gmail.com>
2018-08-06 20:36:00 -0500

    The most basic of basics

---
+++ src/main/java/com/example/catalog/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.catalog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "")
+    @ResponseBody
+    public String index() {
+        return "Hello World!!!!!";
+    }
+
+}
